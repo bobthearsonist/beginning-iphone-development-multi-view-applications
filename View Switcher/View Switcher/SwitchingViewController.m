41a8dcf7a7fea09eaeb6c1017ecf6fdc2e18aca7
Martin Perry <martinperry@gatech.edu>
2016-09-07 18:26:22 -0400

    Added views and toolbar

--- View Switcher/View Switcher/SwitchingViewController.m
+++ View Switcher/View Switcher/SwitchingViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "SwitchingViewController.h"
+#import "YellowViewController.h"
+#import "BlueViewController.h"
 
 @interface SwitchingViewController ()
-
+@property (strong,nonatomic) YellowViewController* yellowViewController;
+@property (strong,nonatomic) BlueViewController* blueViewController;
 @end
 
 @implementation SwitchingViewController
@@ -22,6 +25,9 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)switchViews:(id)sender {
 }
 
 @end
---
+++ View Switcher/View Switcher/YellowViewController.h
@@ -0,0 +1,13 @@
+//
+//  YellowViewController.h
+//  View Switcher
+//
+//  Created by admin on 9/7/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YellowViewController : UIViewController
+
+@end
