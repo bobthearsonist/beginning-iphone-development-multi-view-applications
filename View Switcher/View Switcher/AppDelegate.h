b7d2537a525e56a13c6f5e36a8f1ca100ff7c073
Martin Perry <martinperry@gatech.edu>
2016-09-07 18:05:42 -0400

    Created workspace and project

---
+++ View Switcher/View Switcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  View Switcher
+//
+//  Created by admin on 9/5/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ View Switcher/View Switcher/SwitchingViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  View Switcher
+//
+//  Created by admin on 9/5/16.
+//  Copyright © 2016 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwitchingViewController : UIViewController
+
+
+@end
+
